80539aa2f2c8e3a9e9d253a9b2a414f32c9324a8
junghee.kim <wjdgml3834@naver.com>
2022-02-13 21:35:56 +0900

    ✨ 14.7.3 categoires에서 NavLink 사용

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,4 +1,5 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
 
 const categories = [
   {
@@ -37,8 +38,9 @@ const Categories = ({ onSelect, category }) => {
       {categories.map((item) => (
         <Category
           key={item.name}
-          active={category === item.name}
-          onClick={() => onSelect(item.name)}
+          activeClassName="active"
+          exact={item.name === "all"}
+          to={item.name === "all" ? "/" : `/${item.name}`}
         >
           {item.text}
         </Category>
@@ -60,7 +62,7 @@ const CategoriesBlock = styled.div`
   }
 `;
 
-const Category = styled.div`
+const Category = styled(NavLink)`
   font-size: 1.125rem;
   cursor: pointer;
   white-space: pre;
@@ -72,16 +74,14 @@ const Category = styled.div`
     color: #495057;
   }
 
-  ${(props) =>
-    props.active &&
-    css`
-      font-weight: 600;
-      border-bottom: 2px solid #22b8cf;
-      color: #22b8cf;
-      &:hover {
-        color: #3bc9db;
-      }
-    `}
+  &.active {
+    font-weight: 600;
+    border-bottom: 2px solid #22b8cf;
+    color: #22b8cf;
+    &:hover {
+      color: #3bc9db;
+    }
+  }
 
   & + & {
     margin-left: 1rem;
