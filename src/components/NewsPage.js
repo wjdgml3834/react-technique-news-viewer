09fe8639146c641af93e12fb729b656002533daf
junghee.kim <wjdgml3834@naver.com>
2022-02-13 21:29:46 +0900

    ✨ 14.7.2 NewsPage 생성

---
+++ src/components/NewsPage.js
@@ -0,0 +1,15 @@
+import Categories from "./Categories";
+import NewsList from "./NewsList";
+
+const NewsPage = ({ match }) => {
+  const category = match.params.category || "all";
+
+  return (
+    <>
+      <Categories />
+      <NewsList category={category} />
+    </>
+  );
+};
+
+export default NewsPage;
